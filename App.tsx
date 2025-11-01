c8e385616643b90e768ece419671e52a3e505653
CodeX60 <151678852+AzizSaddique@users.noreply.github.com>
2025-11-01 13:28:14 -0700

    New project password generator

--- App.tsx
+++ App.tsx
@@ -1,45 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+//form validation
+import * as Yup from 'yup'
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be min 4 character')
+    .max(16, 'Should be max 16 character')
+    .required('Length is required')
+})
 
+export default function App() {
+
+  const [password, setPassword] = useState('')
+  const [passGenerator, setpassGenerator]= useState(false)
+  const [lowerCase, setLowerCase]= useState(true)
+  const [upperCase, setUpperCase]= useState(false)
+  const [nummbers, setNumbers]= useState(false)
+  const [symbols, setSymbols]= useState(false)
+
+  const generatePasswordString= (passwordLength: number)=>{
+    let characterList=''
+    const uperCaseChars='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lowerCaseChars='abcdefghijklmnopqrstuvwxyz'
+    const digitsChars='123456789'
+    const specialChars='!@#$%^&*()_+-={}[]|'
+
+    if (upperCase) {
+      characterList += uperCaseChars
+    }
+    if (lowerCase) {
+      characterList += lowerCaseChars
+    }
+    if (nummbers) {
+      characterList += digitsChars
+    }
+    if (symbols) {
+      characterList += specialChars
+    }
+
+    const passwordResults=createPassword(characterList, passwordLength)
+    setPassword(passwordResults)
+    setpassGenerator(true)
+  } 
+  const createPassword= (character:string, passwordLength: number)=>{
+    let result =''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex= Math.round(Math.random()*character.length)
+      result +=character.charAt(characterIndex)
+      
+    }
+    return result
+    console.log('Aziz')
+  }
+  const resetPasswordState=()=>{
+    setPassword('')
+    setpassGenerator(false)
+    setLowerCase(true)
+    setUpperCase(false)
+    setNumbers(false)
+    setSymbols(false)
+  }
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
